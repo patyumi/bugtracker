dd95626d780c8a3768311e50c0cc976973c342b4
Patrícia Yumi <pati.yumi@yahoo.com.br>
2019-11-10 08:58:07 -0300

    aula 3

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
 const GoogleSpreadSheet = require("google-spreadsheet");
+const { promisify } = require("util");
+
 const credentials = require("./bugtracker.json");
 
 const app = express();
@@ -19,31 +21,28 @@ app.get("/", (req, res) => {
   res.render("home");
 });
 
-app.post("/", (req, res) => {
-  const doc = new GoogleSpreadSheet(docId);
-  doc.useServiceAccountAuth(credentials, err => {
-    if (err) {
-      console.log("Não foi possível abrir a planilha");
-    } else {
-      console.log("Planilha aberta");
-      doc.getInfo((err, info) => {
-        const worksheet = info.worksheets[worksheetIndex];
-        worksheet.addRow(
-          {
-            name: req.body.name,
-            email: req.body.email,
-            issueType: req.body.issueType,
-            howToReproduce: req.body.howToReproduce,
-            expectedOutput: req.body.expectedOutput,
-            receivedOutput: req.body.receivedOutput
-          },
-          err => {
-            res.send("Bug reportado com sucesso!");
-          }
-        );
-      });
-    }
-  });
+app.post("/", async (req, res) => {
+  try {
+    const doc = new GoogleSpreadSheet(docId);
+    await promisify(doc.useServiceAccountAuth)(credentials);
+    const info = await promisify(doc.getInfo)();
+    const worksheet = info.worksheets[worksheetIndex];
+    await promisify(worksheet.addRow)({
+      name: req.body.name,
+      email: req.body.email,
+      issueType: req.body.issueType,
+      source: req.body.source || "direct",
+      howToReproduce: req.body.howToReproduce,
+      expectedOutput: req.body.expectedOutput,
+      receivedOutput: req.body.receivedOutput,
+      userAgent: req.body.userAgent,
+      userDate: req.body.userDate
+    });
+    res.render("sucesso");
+  } catch (err) {
+    res.send("Erro ao enviar formulário");
+    console.log(err);
+  }
 });
 
 app.listen(3000, err => {
--- testar-planilha.js
+++ testar-planilha.js
@@ -1,6 +1,21 @@
 const GoogleSpreadSheet = require("google-spreadsheet");
 const credentials = require("./bugtracker.json");
+const { promisify } = require("util");
 
+const addRowToSheet = async () => {
+  const doc = new GoogleSpreadSheet(
+    "1_mFsEKnWLSbb4FjNAbhtv8pFKc4J8vdkNPJXLs9JIZI"
+  );
+  await promisify(doc.useServiceAccountAuth)(credentials);
+  console.log("Planilha aberta");
+  const info = await promisify(doc.getInfo)();
+  const worksheet = info.worksheets[0];
+  await promisify(worksheet.addRow)({ name: "Tulio", email: "test" });
+};
+
+addRowToSheet();
+
+/*
 const doc = new GoogleSpreadSheet(
   "1_mFsEKnWLSbb4FjNAbhtv8pFKc4J8vdkNPJXLs9JIZI"
 );
@@ -15,3 +30,4 @@ doc.useServiceAccountAuth(credentials, err => {
     });
   }
 });
+*/
